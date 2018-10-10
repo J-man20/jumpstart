1322eefa902e1c21bdc89341db4cc9f058bad647
J-man20 <prcrown4life@gmail.com>
2018-10-09 21:48:06 -0400

    Able to hide navbar

--- resources/js/main.js
+++ resources/js/main.js
@@ -12,18 +12,16 @@ $(document).ready(function(){
 
 
 var hideNavBar = 0;
-
 $(window).scroll(function(){
 var scrollTop = $(this).scrollTop();
-
 if (scrollTop - hideNavBar > 50){
   var navBarHeight = $('.navbar').css('height');
-
+  $('.navbar').animate({top: '-' + navBarHeight},150);
+  hideNavBar = scrollTop;
+} else if (hideNavBar - scrollTop > 50){
+  $('.navbar').animate({top: '0px'}, 150);
+  hideNavBar = scrollTop;
 }
-
-$(navBarHeight).animate({top: '-' + navBarHeight},150);
-
-$(hideNavBar) = $(scrollTop);
 
 });
 
